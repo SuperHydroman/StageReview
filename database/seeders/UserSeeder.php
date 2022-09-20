38807f170bf1ca9552703c55911a618c459df86f
Gideon van den Herik <g.vdherik@live.nl>
2022-09-20 13:23:38 +0200

    First Commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'id' => 1,
+            'name' => env('APP_NAME'),
+            'email' => env('APP_MAIL'),
+            'password' => Hash::make(env('APP_PASSWORD'))
+        ]);
+    }
+}
