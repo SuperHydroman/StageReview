d6d1c5049dde419a11c41c75b76a96c7b14cf4d5
daveleeggangers <daveleeggangers123@hotmail.com>
2022-10-06 10:50:28 +0200

    RoleSeeder changes!

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,7 +16,7 @@
                         {{ __('Bedrijven') }}
                     </x-nav-link>
                     <x-nav-link :href="route('roles.index')" :active="request()->routeIs('roles.index')">
-                        {{ __('Role') }}
+                        {{ __('Rollen') }}
                     </x-nav-link>
                     <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
                         {{ __('Gebruikers') }}
--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -3,25 +3,34 @@
 
         {{-- <x-header> imports the header component into this view. --}}
         <x-header :route="__('companies.create')">
-            <x-slot:title> {{ __('Role') }} </x-slot>
-    <i class="fa-solid fa-plus pr-2"></i> {{ __('Role toevoegen') }}
+            <x-slot:title> {{ __('Rollen') }} </x-slot>
+    <i class="fa-solid fa-plus pr-2"></i> {{ __('Rol toewijzen') }}
         </x-header>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <table>
-                <tr>
-                    <td>Role id</td>
-                    <td>Role Name</td>
-                </tr>
-                @foreach($roles as $role)
-                    <tr>
-                        <td>{{$role['RoleID']}}</td>
-                        <td>{{$role['RoleName']}}</td>
-                    </tr>
-                @endforeach
-            </table>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-2 bg-white border-b border-gray-200">
+                    <div class="border-solid border-2 border-black rounded-md text-center w-1/2 my-1">
+                        @foreach($roles as $role)
+                            <tr>
+                                <td>{{$role->id}}</td>
+                                <td>{{$role->name}}</td>
+                            </tr>
+                        @endforeach
+                    </div>
+                    <div class="border-solid border-2 border-black rounded-md text-center w-1/2">
+                        {{$role->id}}
+                    </div>
+                    <div class="border-solid border-2 border-black rounded-md text-center w-1/2">
+                        {{$role->id}}
+                    </div>
+                    <div class="border-solid border-2 border-black rounded-md text-center w-1/2">
+                        {{$role->id}}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </x-app-layout>
